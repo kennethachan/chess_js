c50454af2cfe9385a37ec136127a3dc431fa96f4
kennethachan <kennethandrechan@gmail.com>
2023-07-28 21:08:01 -0400

    checkerboard pattern and pieces rendered

--- app.js
+++ app.js
@@ -3,4 +3,133 @@ const playerDisplay = document.querySelector("#player")
 const infoDisplay = document.querySelector("#info-display")
 const width = 8
 
-const startPieces = []
+const startPieces = [
+  rook,
+  knight,
+  bishop,
+  queen,
+  king,
+  bishop,
+  knight,
+  rook,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  rook,
+  knight,
+  bishop,
+  queen,
+  king,
+  bishop,
+  knight,
+  rook,
+]
+
+// function createBoard() {
+//   startPieces.forEach((startPiece, i) => {
+//     const square = document.createElement("div")
+//     square.classList.add("square")
+//     square.innerHTML = startPiece
+//     square.setAttribute("square-id", i)
+
+//     const row = Math.floor((63 - i) / 8) + 1
+//     if (row % 2 === 0) {
+//       square.classList.add(i % 2 === 0 ? "beige" : "brown")
+//     } else {
+//       square.classList.add(i % 2 === 0 ? "brown" : "beige")
+//     }
+
+//     if (i <= 15) {
+//       let firstChild = square.firstChild
+//       if (firstChild && firstChild.firstChild) {
+//         firstChild.firstChild.classList.add("black")
+//       }
+//     }
+
+//     if (i >= 48) {
+//       let firstChild = square.firstChild
+//       if (firstChild && firstChild.firstChild) {
+//         firstChild.firstChild.classList.add("white")
+//       }
+//     }
+
+//     gameBoard.append(square)
+//   })
+// }
+
+const createPiece = (piece) => {
+  const pieceDiv = document.createElement("div")
+  pieceDiv.classList.add("piece")
+  pieceDiv.innerHTML = piece
+  return pieceDiv
+}
+
+function createBoard() {
+  startPieces.forEach((startPiece, i) => {
+    const square = document.createElement("div")
+    square.classList.add("square")
+    square.setAttribute("square-id", i)
+
+    const row = Math.floor((63 - i) / 8) + 1
+    if (row % 2 === 0) {
+      square.classList.add(i % 2 === 0 ? "beige" : "brown")
+    } else {
+      square.classList.add(i % 2 === 0 ? "brown" : "beige")
+    }
+
+    if (i <= 15) {
+      square.appendChild(createPiece(startPiece).cloneNode(true))
+    } else if (i >= 48) {
+      square.appendChild(createPiece(startPiece).cloneNode(true))
+    }
+
+    gameBoard.append(square)
+  })
+}
+
+createBoard()
